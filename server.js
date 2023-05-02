bd6d4206ed002ee9fb7a8e478efe82661dd84204
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-05-02 15:17:14 +0300

    add

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 // const DB_HOST = require('./config')
 const DB_HOST = "mongodb+srv://Valerij:yYyX7ophSwjPTr40@cluster0.ibb8iqz.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
-mongoose.connect(DB_HOST).then(app.listen(3000)).catch(e => { console.log(e.message); process.exit(1) });
+mongoose.connect(DB_HOST).then(app.listen(3001)).catch(e => { console.log(e.message); process.exit(1) });
 
 // app.listen(3000, console.log('server running on port 3000'));
